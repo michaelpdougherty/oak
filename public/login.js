16250b5e163baa21b2c253af1acc0f61ea5d68c6
Mike Dougherty <mikedough213@gmail.com>
2019-12-06 21:35:15 -0600

    No more login failure*

--- public/login.js
+++ public/login.js
@@ -1,9 +1,17 @@
 const form = document.getElementById("login-form");
+const usernameInput = document.getElementById("usernameInput")
+const passwordInput = document.getElementById("passwordInput")
 const go = document.getElementById("go");
 const header = document.getElementById("header");
 const err = document.getElementById("err");
 form.onsubmit = () => {
-  header.innerHTML = "LOGGING IN...";
-  go.style.display = "none";
-  err.style.display = "none";
+  if (!form.disabled) {
+    header.innerHTML = "LOGGING IN...";
+    go.style.display = "none";
+    if (err) { err.style.display = "none"; }
+    form.disabled = true;
+    usernameInput.blur();
+    passwordInput.blur();
+    form.blur();
+  }
 };
